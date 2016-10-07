7c4463e9d5fe96d00c08a8dfd82f6b9bd8693826
john206 <retlaw@uw.edu>
2016-10-07 13:49:57 -0700

    Lab2-9 in progress

---
+++ Lab2/Lab2-9/Lab2-9.ino
@@ -0,0 +1,58 @@
+//TMP36 Pin Variables
+int sensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
+                        //the resolution is 10 mV / degree centigrade with a
+                        //500 mV offset to allow for negative temperatures
+ 
+int thresholdTemp;
+
+void setup()
+{
+  Serial.begin(9600);  //Start the serial connection with the computer
+                       //to view the result open the serial monitor 
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  thresholdTemp = 1000;
+}
+ 
+void loop()                     // run over and over again
+{
+  if (Serial.available() > 0) 
+  {
+        // read the incoming byte:
+        int input = Serial.readString().toInt();        
+        
+        thresholdTemp = input;        
+        Serial.println("Threshold temp: " + String(thresholdTemp));    
+  } 
+
+  if ((int)readTemperature() < thresholdTemp)
+  {
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  }
+    
+   delay(1000);                                     
+}
+
+float readTemperature()
+{
+  //getting the voltage reading from the temperature sensor
+   int reading = analogRead(sensorPin);  
+   
+   // converting that reading to voltage, for 3.3v arduino use 3.3
+   float voltage = reading * 5.0;
+   voltage /= 1024.0; 
+   
+   float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                                 //to degrees ((voltage - 500mV) times 100)
+   
+   // now convert to Fahrenheit
+   float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+   Serial.print(temperatureF); Serial.println(" degrees F");
+   return temperatureF;
+}

2db8af9daafdc57ea2f3e64de2c170059db2d3e7
tbdye <thomas@tfenet.com>
2016-10-07 13:31:01 -0700

    Accidentally overwrote 2-7.  Restoring code.

--- Lab2/Lab2-7/Lab2-7.ino
+++ Lab2/Lab2-7/Lab2-7.ino
@@ -2,8 +2,8 @@
 int sensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
                         //the resolution is 10 mV / degree centigrade with a
                         //500 mV offset to allow for negative temperatures
-
-int mode;
+ 
+int thresholdTemp;
 
 void setup()
 {
@@ -13,7 +13,7 @@ void setup()
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 
-  mode = 0;
+  thresholdTemp = 1000;
 }
  
 void loop()                     // run over and over again
@@ -22,44 +22,31 @@ void loop()                     // run over and over again
   {
         // read the incoming byte:
         int input = Serial.readString().toInt();        
-               
-        Serial.println("Reference voltage mode: " + String(input));
-
-        if (!(input == 0 || input == 2 || input == 3))
-        {
-          Serial.println("bad mode");
-        }
-        else
-        {
-          mode = input;
-        }
+        
+        thresholdTemp = input;        
+        Serial.println("Threshold temp: " + String(thresholdTemp));    
   } 
 
-   readTemperature();
+  if ((int)readTemperature() < thresholdTemp)
+  {
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  }
+    
    delay(1000);                                     
 }
 
-void readTemperature()
+float readTemperature()
 {
-   switch(mode)
-   {
-     case 0:
-       analogReference(DEFAULT);
-       break;
-     case 2:
-      analogReference(INTERNAL1V1);
-      break;
-     case 3:
-      analogReference(INTERNAL2V56);
-      break;
-   }
-   
-   //getting the voltage reading from the temperature sensor
+  //getting the voltage reading from the temperature sensor
    int reading = analogRead(sensorPin);  
    
    // converting that reading to voltage, for 3.3v arduino use 3.3
    float voltage = reading * 5.0;
-   voltage /= 1024.0;
+   voltage /= 1024.0; 
    
    float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
                                                  //to degrees ((voltage - 500mV) times 100)
@@ -67,6 +54,5 @@ void readTemperature()
    // now convert to Fahrenheit
    float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
    Serial.print(temperatureF); Serial.println(" degrees F");
+   return temperatureF;
 }
-
-

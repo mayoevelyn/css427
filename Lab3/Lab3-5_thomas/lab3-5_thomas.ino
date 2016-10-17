a37d3bbb314058025631c4e250a9fad6cc82cfe9
tbdye <thomas@tfenet.com>
2016-10-17 14:00:45 -0700

    Disabled native keypad driver.  Added some sample code for interrupt driven events.

--- Lab3/Lab3-5_thomas/lab3-5_thomas.ino
+++ Lab3/Lab3-5_thomas/lab3-5_thomas.ino
@@ -1,28 +1,42 @@
-#include <Key.h>
-#include <Keypad.h>
+//#include <Key.h>
+//#include <Keypad.h>
+//
+//const byte rows = 4; //four rows
+//const byte cols = 3; //three columns
+//char keys[rows][cols] = {
+//  {'1','2','3'},
+//  {'4','5','6'},
+//  {'7','8','9'},
+//  {'*','0','#'}
+//};
+//byte rowPins[rows] = {18, 17, 16, 15}; //connect to the row pinouts of the keypad
+//byte colPins[cols] = {21, 20, 19}; //connect to the column pinouts of the keypad
+//Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
-void setup() {
-  const byte rows = 4; //four rows
-  const byte cols = 3; //three columns
-  char keys[rows][cols] = {
-    {'1','2','3'},
-    {'4','5','6'},
-    {'7','8','9'},
-    {'*','0','#'}
-  };
-  byte rowPins[rows] = {17, 16, 15, 14}; //connect to the row pinouts of the keypad
-  byte colPins[cols] = {20, 19, 18}; //connect to the column pinouts of the keypad
-  Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
+int pin = 21;
+volatile int state = LOW;
 
-  void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+void setup()
+{
+  pinMode(pin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(pin), ISR, mode);
+  
+  //Serial.begin(9600);
 }
 
-void loop() {
-  char key = keypad.getKey();
+void loop()
+{
+  digitalWrite(pin, state);
 
-  if (key != NO_KEY){
-   Serial.println(key);
-  }
+//  char key = keypad.getKey();
+//
+//  if (key != NO_KEY)
+//  {
+//    Serial.println(key);
+//  }
+}
+
+void blink()
+{
+  state = !state;
 }

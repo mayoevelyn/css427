fdc116aeffca020e5ee58e833c84298b7862894e
tbdye <thomas@tfenet.com>
2016-10-22 12:04:53 -0700

    Arduino sender and receiver sketches to remotely turn on and off an LED.

---
+++ Project/receiver/receiver.ino
@@ -0,0 +1,24 @@
+char input;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(53, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available())
+  {
+    input = Serial.read();
+
+    if (input == '0')
+    {
+      digitalWrite(53, LOW);
+    }
+    if (input == '1')
+    {
+      digitalWrite(53, HIGH);
+    }
+  }
+}
---
+++ Project/sender/sender.ino
@@ -0,0 +1,12 @@
+void setup() 
+{
+    Serial.begin(9600);
+}
+
+void loop() 
+{
+    Serial.println('0');
+    delay(500);
+    Serial.println('1');
+    delay(500);
+}

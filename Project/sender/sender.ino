087d96084aacc389b3f7c71115fb4a17470e4a9d
tbdye <thomas@tfenet.com>
2016-10-28 13:28:54 -0700

    Committing XBee sender receiver code that works.

--- Project/sender/sender.ino
+++ Project/sender/sender.ino
@@ -1,37 +1,130 @@
+/**
+ * Copyright (c) 2009 Andrew Rapp. All rights reserved.
+ *
+ * This file is part of XBee-Arduino.
+ *
+ * XBee-Arduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XBee-Arduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XBee-Arduino.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 #include <XBee.h>
 
-//Sends a ZB TX request with the value of analogRead(pin5) and checks the
-//status response for success
+/*
+This example is for Series 1 XBee
+Sends a TX16 or TX64 request with the value of analogRead(pin5) and checks the status response for success
+Note: In my testing it took about 15 seconds for the XBee to start reporting success, so I've added a startup delay
+*/
 
-// create the XBee object
 XBee xbee = XBee();
 
+unsigned long start = millis();
+
+// allocate two bytes for to hold a 10-bit analog reading
 uint8_t payload[] = { 0, 0 };
+//uint8_t payload = 0;
 
-// SH + SL Address of receiving XBee
-XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x40E3CD1E);
-ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
+// with Series 1 you can use either 16-bit or 64-bit addressing
 
-void setup() 
-{
-    Serial.begin(9600);
-    xbee.setSerial(Serial);
+// 16-bit addressing: Enter address of remote XBee, typically the coordinator
+//Tx16Request tx = Tx16Request(0x1234, payload, sizeof(payload));
+
+// 64-bit addressing: This is the SH + SL address of remote XBee
+XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x40F9CE00);
+// unless you have MY on the receiving radio set to FFFF, this will be received as a RX16 packet
+Tx64Request tx = Tx64Request(addr64, payload, sizeof(payload));
+
+TxStatusResponse txStatus = TxStatusResponse();
+
+char output = '0';
+
+int statusLed = 47;
+int errorLed = 53;
+
+void flashLed(int pin, int times, int wait) {
+    
+    for (int i = 0; i < times; i++) {
+      digitalWrite(pin, HIGH);
+      delay(wait);
+      digitalWrite(pin, LOW);
+      
+      if (i + 1 < times) {
+        delay(wait);
+      }
+    }
 }
 
-void loop() 
-{
-    // break down 10-bit reading into two bytes and place in payload
-    int value = 0;
-    payload[0] = value >> 8 & 0xff;
-    payload[1] = value & 0xff;
-    xbee.send(zbTx);
-    
-    delay(500);
-
-    value = 1;
-    payload[0] = value >> 8 & 0xff;
-    payload[1] = value & 0xff;
-    xbee.send(zbTx);
-    
-    delay(500);
+void setup() {
+  pinMode(statusLed, OUTPUT);
+  pinMode(errorLed, OUTPUT);
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
 }
+
+void loop() {
+   // start transmitting after a startup delay.  Note: this will rollover to 0 eventually so not best way to handle
+    if (millis() - start > 15000) {
+      // break down 10-bit reading into two bytes and place in payload
+      payload[0] = output & 0xff;
+      payload[1] = output >> 8 & 0xff;
+      
+      //payload = pin5;
+      
+      xbee.send(tx);
+
+      // flash TX indicator
+      flashLed(statusLed, 1, 100);
+    }
+  
+    // after sending a tx request, we expect a status response
+    // wait up to 5 seconds for the status response
+    if (xbee.readPacket(5000)) {
+        // got a response!
+
+        // should be a znet tx status            	
+    	if (xbee.getResponse().getApiId() == TX_STATUS_RESPONSE) {
+    	   xbee.getResponse().getTxStatusResponse(txStatus);
+    		
+    	   // get the delivery status, the fifth byte
+           if (txStatus.getStatus() == SUCCESS) {
+            	// success.  time to celebrate
+             	flashLed(statusLed, 5, 50);
+           } else {
+            	// the remote XBee did not receive our packet. is it powered on?
+             	flashLed(errorLed, 3, 500);
+           }
+        }      
+    } else if (xbee.getResponse().isError()) {
+      //nss.print("Error reading packet.  Error code: ");  
+      //nss.println(xbee.getResponse().getErrorCode());
+      flashLed(errorLed, 3, 100);
+    } else {
+      // local XBee did not provide a timely TX Status Response.  Radio is not configured properly or connected
+      flashLed(errorLed, 2, 50);
+    }
+
+    setOutput();
+    delay(1000);
+}
+
+void setOutput()
+{
+    if (output == '0')
+    {
+        output = '1';
+    }
+    else
+    {
+        output = '0';
+    }
+}
+

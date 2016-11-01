cf7e52cb240ae7e63557b4a52fa31d606d1a381b
john206 <retlaw@uw.edu>
2016-11-01 15:30:15 -0700

    pushing

--- Project/charArraySender/charArraySender.ino
+++ Project/charArraySender/charArraySender.ino
@@ -5,14 +5,16 @@
 
 #include <XBee.h>
 
-char buffer[128];
+// Payload
+uint8_t payload[100];
+bool outputHello;
 
 // Initialize radio object
 XBee xbee = XBee();
 
 // 64-bit addressing: This is the SH + SL address of remote XBee
 XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x40E3CD0F);
-//Tx64Request tx = Tx64Request(addr64, payload, sizeof(payload));
+Tx64Request tx = Tx64Request(addr64, payload, sizeof(payload));
 TxStatusResponse txStatus = TxStatusResponse();
 
 // LED pin assignments
@@ -22,37 +24,35 @@ int errorLed = 53;
 // Setup
 void setup()
 {
+    outputHello = true;
+    
     // Prepare diagnostic leds
     pinMode(statusLed, OUTPUT);
     pinMode(errorLed, OUTPUT);
     
     // Prepare serial connections
     Serial.begin(9600);
-    delay(250);
-    Serial2.begin(9600);
     xbee.setSerial(Serial);
     
     // Allow radio to fully boot and establish connection to remote
     delay(10000);
 
     // Check LEDs
-    flashLed(errorLed, 3, 50);
-    flashLed(statusLed, 3, 50);
+    for (int i = 0; i < 10; i++)
+    {
+        flashLed(errorLed, 1, 100);
+        flashLed(statusLed, 1, 100);
+    }
 }
 
 // Loop
 void loop()
 {
+    //initBuffer();
     
-    packHello();
-    Tx64Request zbtx = Tx64Request(addr64, (uint8_t *)buffer, strlen(buffer));
-    xbee.send(zbtx);
-    delay(2000);
-
-    packGoodbye();
-    zbtx = Tx64Request(addr64, (uint8_t *)buffer, strlen(buffer));
-    xbee.send(zbtx);
-    delay(2000);
+    //pack();
+    //Tx64Request zbtx = Tx64Request(addr64, (uint8_t *)buffer, strlen(buffer));
+    xbee.send(tx);
 
     // flash TX indicator for each byte in payload
     flashLed(statusLed, 4, 25);
@@ -93,7 +93,6 @@ void loop()
         flashLed(errorLed, 2, 50);
     }
 
-    //setOutput();
     delay(1000);
 }
 
@@ -115,23 +114,43 @@ void flashLed(int pin, int times, int wait)
 
 void packHello()
 {
-    buffer[0] = 'h';
-    buffer[1] = 'e';
-    buffer[2] = 'l';
-    buffer[3] = 'l';
-    buffer[4] = '0';
-    buffer[5] = '\r';
+    payload[0] = 'h' & 0xff;
+    payload[1] = 'e' & 0xff;
+    payload[2] = 'l' & 0xff;
+    payload[3] = 'l' & 0xff;
+    payload[4] = 'o' & 0xff;
 }
 
 void packGoodbye()
 {
-    buffer[0] = 'g';
-    buffer[1] = 'o';
-    buffer[2] = 'o';
-    buffer[3] = 'd';
-    buffer[4] = 'b';
-    buffer[5] = 'y';
-    buffer[6] = 'e';
-    buffer[7] = '\r';
+    payload[0] = 'g' & 0xff;
+    payload[1] = 'o' & 0xff;
+    payload[2] = 'o' & 0xff;
+    payload[3] = 'd' & 0xff;
+    payload[4] = 'b' & 0xff;
+    payload[5] = 'y' & 0xff;
+    payload[6] = 'e' & 0xff;
+}
+
+void pack()
+{
+    if (outputHello)
+    {
+        packHello();
+    }
+    else
+    {
+        packGoodbye();
+    }
+
+    outputHello = !outputHello;
+}
+
+void initBuffer()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        payload[i] = 0;
+    }
 }
 

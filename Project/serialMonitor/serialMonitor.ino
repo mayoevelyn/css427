7c864b034bedc9908d4562a87ef39abdc37dc6cd
tbdye <thomas@tfenet.com>
2016-11-03 03:34:19 -0700

    Moved old development into archive folder

---
+++ Project/serialMonitor/serialMonitor.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+
+// Setup device interconnect over serial
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup() {
+    Serial.begin(9600);
+    
+    delay(1000);
+    Serial.println("Booting Arduino Micro");
+
+    // Set the data rate for the SoftwareSerial port
+    mySerial.begin(4800);
+}
+
+void loop() {
+    if (mySerial.available())
+    {
+        Serial.write(mySerial.read());
+        delay(1);
+    }
+}

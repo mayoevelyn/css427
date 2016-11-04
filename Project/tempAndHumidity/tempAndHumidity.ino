f68d00ad023c51671792e705f1e75a54b48e1459
tbdye <thomas@tfenet.com>
2016-11-04 10:35:18 -0700

    Finished tempAndHumidity sensor code

---
+++ Project/tempAndHumidity/tempAndHumidity.ino
@@ -0,0 +1,49 @@
+// Library info:  https://github.com/adafruit/Adafruit_DHT_Unified
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN            53        // Digital pin for data out
+#define DHTTYPE           DHT11     // DHT 11
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+void setup() {
+    Serial.begin(9600);
+
+    dht.begin();
+}
+
+void loop() {
+    // Delay between measurements.
+    delay(1000);
+    
+    // Get temperature event and print its value.
+    sensors_event_t event;  
+    dht.temperature().getEvent(&event);
+    
+    if (isnan(event.temperature))
+    {
+        Serial.println("Error reading temperature!");
+    }
+    else
+    {
+        Serial.print("Temperature: ");
+        Serial.print(event.temperature);
+        Serial.println(" *C");
+    }
+    
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity))
+    {
+        Serial.println("Error reading humidity!");
+    }
+    else
+    {
+        Serial.print("Humidity: ");
+        Serial.print(event.relative_humidity);
+        Serial.println("%");
+    }
+}

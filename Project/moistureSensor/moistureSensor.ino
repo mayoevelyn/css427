5e13a7231fb10b07b0e627596f2019fde072eee9
tbdye <thomas@tfenet.com>
2016-11-04 14:19:46 -0700

    Finished soil moisture sensor code

---
+++ Project/moistureSensor/moistureSensor.ino
@@ -0,0 +1,14 @@
+const int VAL_PROBE = 0; // Analog pin 0
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop() {
+    int moisture = analogRead(VAL_PROBE);
+    Serial.println(moisture);
+
+    delay(1000);
+}
+

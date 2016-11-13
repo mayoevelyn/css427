3593876278bec7ef4e21db36b5296c71107b1163
John <evantirrel@gmail.com>
2016-11-13 12:35:37 -0800

    Tested dynamic updating of shared.pkl file

--- Project/webServer/demo/pickleWrite.py
+++ Project/webServer/demo/pickleWrite.py
@@ -1,5 +1,8 @@
 import pickle
 
 shared = {"Foo":"Bar", "Honey":"Badger", "Shit":"Storm"}
+shared["Foo"] = "Updated Bar Value"
+shared["NewKey"] = "New Key"
+shared["DynamicTest"] = "Test"
 fp = open("shared.pkl", "w")
 pickle.dump(shared, fp)
